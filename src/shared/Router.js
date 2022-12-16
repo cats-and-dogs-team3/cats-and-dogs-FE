f982715f3f9a58f3c5d8dbe320c757842b082e4a
elderanni <elderanni@naver.com>
2022-12-16 21:48:34 +0900

    LoginPage/ Header 기초작업 + MainPage

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -3,14 +3,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import DetailPage from "../pages/DetailPage";
 import UploadPostPage from "../pages/UploadPostPage";
+import LoginPage from "../pages/LoginPage";
 
 const Router = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainPage/>} />
+          <Route path="/" element={<MainPage />} />
           <Route path="/post/:postId" element={<DetailPage />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/upload" element={<UploadPostPage />} />
           <Route path="/update/:postId" element={<UploadPostPage />} />
         </Routes>
