896f0d805907e8e8467979a5d61f566fef596d20
mugyeol <htpadkorik@naver.com>
2022-12-22 20:45:38 +0900

    refactor: clean up code

--- src/pages/EditPage.js
+++ src/pages/EditPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Layout from "../components/Layout";
 import styled from "styled-components";
 import Mybutton from "../components/ui/MyButton";
